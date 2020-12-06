9f291f121370dd743db71ec2a8890f57b5a470cd
Golovolastik <yankovich.aliaksei@gmail.com>
2020-12-07 00:55:49 +0300

    main-refactoring

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -2,26 +2,40 @@ import sys
 
 import pygame
 
+from settings import Settings
+from ship import Ship
+
 class AlienInvasion:
     """Класс для управления ресурсами и поведением игры."""
     
     def __init__(self):
         """Инициализирует игру и создает игровые ресурсы."""
         pygame.init()
-        
-        self.screen = pygame.display.set_mode((1200, 800))
-        pygame.display.set_caption("Alien Invasion")
+        self.settings = Settings()
+        pygame.display.set_caption('Alien Invasion')
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height))
+        self.ship = Ship(self)
         
     def run_game(self):
         """Запуск основного цикла игры."""
         while True:
-            # Отслеживание событий клавиатуры и мыши.
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
+            self._check_events()
+            self._update_screen()
+    
+    def _check_events(self):
+        """Обрабатывает нажатия клавиш и события мыши."""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+    def _update_screen(self):
+        """Обновляет изображения на экране и отображает новый экран."""
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
                     
-            # Отображение последнего прорисованного экрана.
-            pygame.display.flip()
+        # Отображение последнего прорисованного экрана.
+        pygame.display.flip()
             
 if __name__ == '__main__':
     # Создание экземпляра и запуск игры.
