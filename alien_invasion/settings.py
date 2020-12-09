c4629c7db2fa4fcad9a9236482a1d0e5840ed6af
David Lynch <yankovich.aliaksei@gmail.com>
2020-12-09 21:09:01 +0300

    ref-some updates

--- alien_invasion/settings.py
+++ alien_invasion/settings.py
@@ -1,12 +1,14 @@
+import pygame
+
 class Settings():
     """Класс для хранения всех настроек игры Alien Invasion"""
 
     def __init__(self):
         """Инициализирует настройки игры"""
+        pygame.display.set_caption('Alien Invasion')
+
         # Параметры экрана
-        self.screen_width = 800
-        self.screen_height = 600
         self.bg_color = (230, 230, 230)
         
         # Настройки корабля
-        self.ship_speed = 1.5
+        self.ship_speed = 5
--- alien_invasion/ship.py
+++ alien_invasion/ship.py
@@ -2,6 +2,7 @@ import pygame
 
 class Ship():
     """Класс для управления кораблем."""
+    
     def __init__(self,ai_game):
         """Инициализирует корабль и задает его начальную позицию"""
         self.screen = ai_game.screen
