1c7ca508074983d259a2a3af8ea9c03995c21b73
David Lynch <yankovich.aliaksei@gmail.com>
2020-12-11 20:49:03 +0300

    main-fire add

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -1,9 +1,9 @@
 import sys
 
 import pygame
-
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
 
 class AlienInvasion:
     """Класс для управления ресурсами и поведением игры."""
@@ -16,13 +16,15 @@ class AlienInvasion:
         self.screen_height = self.screen.get_rect().height
         self.settings = Settings()
         self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
         
     def run_game(self):
         """Запуск основного цикла игры."""
         while True:
             self._check_events()
             self.ship.update()
-            self._update_screen()
+            self._update_bullets()            
+            self._update_screen()            
     
     def _check_events(self):
         """Обрабатывает нажатия клавиш и события мыши."""
@@ -40,9 +42,11 @@ class AlienInvasion:
             self.ship.moving_right = True
         if event.key == pygame.K_LEFT:
             self.ship.moving_left = True
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
         elif event.key == pygame.K_q:
                 sys.exit()
-                
+                q
     def _check_keyup_events(self, event):
         """Реагирует на отпускание клавиш"""
         if event.key == pygame.K_RIGHT:
@@ -50,10 +54,27 @@ class AlienInvasion:
         if event.key == pygame.K_LEFT:
             self.ship.moving_left = False
 
+    def _fire_bullet(self):
+        """Содание нового снаряда и включение его в группу bullets."""
+        if len(self.bullets) < self.settings.bullets_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
+
+    def _update_bullets(self):
+        """Обновляет позиции экрана и удаляет старые снаряды."""
+        self.bullets.update()
+        # Удаление снарядов, вышедших за край экрана.
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
+        
+
     def _update_screen(self):
         """Обновляет изображения на экране и отображает новый экран."""
         self.screen.fill(self.settings.bg_color)
         self.ship.blitme()
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
                     
         # Отображение последнего прорисованного экрана.
         pygame.display.flip()
--- alien_invasion/settings.py
+++ alien_invasion/settings.py
@@ -12,3 +12,10 @@ class Settings():
         
         # Настройки корабля
         self.ship_speed = 5
+
+        # Параметры снаряда
+        self.bullet_speed = 3
+        self.bullet_height = 12
+        self.bullet_width = 5
+        self.bullet_color = (60, 60, 60)
+        self.bullets_allowed = 4
