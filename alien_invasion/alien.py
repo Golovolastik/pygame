a13d701913eea8ef12afdfbfa0355b2293fea479
David Lynch <yankovich.aliaksei@gmail.com>
2020-12-13 20:52:02 +0300

    main-aliens moving

--- alien_invasion/alien.py
+++ alien_invasion/alien.py
@@ -8,6 +8,7 @@ class Alien(Sprite):
         """Инициализирует пришельца и задает его начальную позицию."""
         super().__init__()
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
 
         # Загрузка изображения пришельца и назначение атрибута rect
         self.image = pygame.image.load('images/alien.bmp')
@@ -23,3 +24,14 @@ class Alien(Sprite):
     def draw_alien(self):
         """Вывод пришельцев на экран."""
         self.screen.blit(self.image, self.rect)
+
+    def update(self):
+        """Перемещает пришельца вправо или влево."""
+        self.x += (self.settings.alien_speed * self.settings.fleet_direction)
+        self.rect.x = self.x
+    
+    def check_edges(self):
+        """Возвращает True, если пришелец находится у края экрана."""
+        screen_rect = self.screen.get_rect()
+        if self.rect.right >= screen_rect.right or self.rect.left <= 0:
+            return True
